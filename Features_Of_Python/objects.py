cf4e2af82d474b385ad31782a01af2bf9fb9ebbb
Pedro Henrique <trabalho.prog.pedro@gmail.com>
2025-03-22 00:50:59 -0300

    Objects, Values and Types

---
+++ Features_Of_Python/objects.py
@@ -0,0 +1,19 @@
+# In python, everything is an object. Objects are instances of types/classes,
+# and they hold values.
+# 
+#   Each object has:
+#     - An identity (unique and constrant, like a memory address).
+#     - A type (defines the operations that can be performed on the object).
+#     - A value (the data the object holds).
+    
+#Example: Object, Values and Types
+
+x = 42 #object of type int with value 42
+y = "Hello" #object of type str with value "Hello"
+z = [1, 2, 3] #object of type list with value [1, 2, 3]
+
+print(type(x)) #output: class = int
+
+print(type(y)) #output: class = str
+
+print(type(z)) #output: class = list
